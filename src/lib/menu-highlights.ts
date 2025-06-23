55b9bf9f30490982bb018ec87cc971ea420afe97
Abdullah Morrison <abdullahmorrison@gmail.com>
2025-06-23 18:14:29 -0400

    added menu-highlight and testamonial data

---
+++ src/lib/menu-highlights.ts
@@ -0,0 +1,36 @@
+const menuHighlights = [
+  {
+    "src": "/menu-highlights/1.jpg",
+    "alt": "Menu Highlight 1"
+  },
+  {
+    "src": "/menu-highlights/2.jpg",
+    "alt": "Menu Highlight 2"
+  },
+  {
+    "src": "/menu-highlights/3.jpg",
+    "alt": "Menu Highlight 3"
+  },
+  {
+    "src": "/menu-highlights/4.jpg",
+    "alt": "Menu Highlight 4"
+  },
+  {
+    "src": "/menu-highlights/5.jpg",
+    "alt": "Menu Highlight 5"
+  },
+  {
+    "src": "/menu-highlights/6.jpg",
+    "alt": "Menu Highlight 6"
+  },
+  {
+    "src": "/menu-highlights/7.jpg",
+    "alt": "Menu Highlight 7"
+  },
+  {
+    "src": "/menu-highlights/8.jpg",
+    "alt": "Menu Highlight 8"
+  }
+]
+
+export default menuHighlights;
---
+++ src/lib/testamonials.ts
@@ -0,0 +1,24 @@
+const testimonials = [
+  {
+    "name": "Emilie L.",
+    "rating": 5,
+    "testimonial": "Great food and customer services was above and beyond. Moroccan ambiance, family friendly and they have some divers Moroccan goodies as well. I'll definitely go there again, recommended"
+  },
+  {
+    "name": "Amanda Fraser",
+    "rating": 5,
+    "testimonial": "Happy I found this little gem of a place! The owners are incredibly friendly. Ordered chicken tajine. The serving size is good for 2 people. Came with fries, bread and a little side salad. Very flavourful and authentic Moroccan dish. Can’t wait to come back and try their other dishes."
+  },
+  {
+    "name": "Cheng Dong",
+    "rating": 5,
+    "testimonial": "Moul Hanout is a family style homey Moroccan restaurant. The restaurant is filled with authentic Moroccan goodies and vibe, there is a blackboard containting all the restaurant's recommended dishes, honestly, you can't go wrong with any of them. We came as a group of International students, some of them Moroccan, they communicated swiftly with the owner, and what we got was a 5 star Moroccan cuisine experience."
+  },
+  {
+    "name": "Marouane AL",
+    "rating": 5,
+    "testimonial": "The one and only place in Ottawa for you to experience Moroccan Dishes the right way !!! In addition to their generosity, the restaurant owners are very cheerful!!"
+  }
+]
+
+export default testimonials
