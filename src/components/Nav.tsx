a6e4e1487fa3558b148e5650a1f6f93849ba6b0b
Abdullah Morrison <abdullahmorrison@gmail.com>
2025-06-29 13:02:17 -0400

    added shadow to nav + code formatting

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,7 +9,7 @@ export default function Nav() {
 
   return (
     <>
-      <nav className="relative flex items-center justify-between p-6 text-2xl font-bold text-primary">
+      <nav className="relative flex items-center justify-between bg-secondary p-6 text-2xl font-bold text-primary shadow-md">
         <Link href="/" className="text-lg">
           <div className="flex h-15 w-15 flex-col items-center justify-center rounded-[50%] border-2 border-white bg-primary p-2 text-white outline-2 outline-primary md:h-20 md:w-20">
             <span>حلال</span>
@@ -20,19 +20,39 @@ export default function Nav() {
         <div
           className={`${isOpen ? "block" : "max-md:opacity-0"} absolute top-25 left-0 z-50 flex w-full flex-col items-center justify-center gap-4 bg-white p-4 transition-all duration-300 ease-in-out md:static md:w-auto md:flex-row md:items-center md:justify-center md:bg-transparent md:p-0`}
         >
-          <Link href={routes.home} className="hover:underline" onClick={() => setIsOpen(false)}>
+          <Link
+            href={routes.home}
+            className="hover:underline"
+            onClick={() => setIsOpen(false)}
+          >
             Home
           </Link>
-          <Link href={routes.menu} className="hover:underline" onClick={() => setIsOpen(false)}>
+          <Link
+            href={routes.menu}
+            className="hover:underline"
+            onClick={() => setIsOpen(false)}
+          >
             Menu
           </Link>
-          <Link href={routes.grocery} className="hover:underline" onClick={() => setIsOpen(false)}>
+          <Link
+            href={routes.grocery}
+            className="hover:underline"
+            onClick={() => setIsOpen(false)}
+          >
             Grocery
           </Link>
-          <Link href={routes.catering} className="hover:underline" onClick={() => setIsOpen(false)}>
+          <Link
+            href={routes.catering}
+            className="hover:underline"
+            onClick={() => setIsOpen(false)}
+          >
             Catering
           </Link>
-          <Link href={routes.contact} className="hover:underline" onClick={() => setIsOpen(false)}>
+          <Link
+            href={routes.contact}
+            className="hover:underline"
+            onClick={() => setIsOpen(false)}
+          >
             Contact
           </Link>
           <SocialMediaLinks />
