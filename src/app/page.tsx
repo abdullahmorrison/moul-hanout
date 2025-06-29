05cefb88b8b5611015495ff3fda849617a62eafd
Abdullah Morrison <abdullahmorrison@gmail.com>
2025-06-29 13:01:16 -0400

    changed landing page footer to use footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import routes from "@/lib/routes";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -173,26 +174,7 @@ export default function Home() {
         </section>
       </main>
 
-      <footer className="mt-auto flex flex-wrap items-center justify-center gap-10 bg-primary p-10 text-white">
-        <address>
-          <h2 className="mb-4 text-3xl font-semibold">Visit Us</h2>
-          <p>
-            <b>Address:</b> 1581 Bank St, Ottawa, ON K1H 7Z3
-          </p>
-          <p>
-            <b>Hours</b>: We Are Open Everyday from 11 AM - 11 PM
-          </p>
-          <p>
-            <b>Phone:</b> <a href="tel:+6132601212">(613) 260-1212</a>
-          </p>
-        </address>
-        <iframe
-          width="500"
-          height="350"
-          loading="lazy"
-          src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJsQM-FwAGzkwRX382EA2eW7Y&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`}
-        ></iframe>
-      </footer>
+      <Footer />
     </div>
   );
 }
