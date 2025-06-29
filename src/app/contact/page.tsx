49254fbc7e87a02a518dfbb0f1dec86eb4d5791f
Abdullah Morrison <abdullahmorrison@gmail.com>
2025-06-29 13:00:16 -0400

    created contact page frontend

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,73 @@
+import Footer from "@/components/Footer";
+
+export default function Contact() {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <h2 className="mt-10 text-center text-5xl font-bold md:my-10">
+        Contact Us
+      </h2>
+
+      <div className="my-6 flex flex-col items-center justify-center gap-6 px-4 md:flex-row md:items-start">
+        <div className="mt-10 flex max-w-xl flex-col space-y-6">
+          <h3 className="text-3xl font-semibold">Become a Franchisee</h3>
+          <p className="max-w-2xl text-lg">
+            If you&apos;re considering becoming a franchisee with Moul Hanout,
+            we&apos;re here to guide you every step of the way. From initial
+            consultation to ongoing support, our team is dedicated to helping
+            you succeed. Fill out the form with your email and message to start
+            this exciting journey together.
+          </p>
+        </div>
+
+        <form className="w-full max-w-3xl rounded-lg bg-secondary p-6 shadow-lg">
+          <div className="mb-4">
+            <label htmlFor="name" className="mb-2 block text-lg font-semibold">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+              className="w-full rounded-lg border border-gray-300 p-3 focus:ring-2 focus:ring-primary focus:outline-none"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="mb-2 block text-lg font-semibold">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              className="w-full rounded-lg border border-gray-300 p-3 focus:ring-2 focus:ring-primary focus:outline-none"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="message"
+              className="mb-2 block text-lg font-semibold"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              rows={5}
+              required
+              className="w-full rounded-lg border border-gray-300 p-3 focus:ring-2 focus:ring-primary focus:outline-none"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="w-full rounded-lg bg-primary py-3 text-white transition-colors duration-200 hover:cursor-pointer hover:bg-primary-dark"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+      <Footer />
+    </div>
+  );
+}
