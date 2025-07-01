5fcc19c1648e3ae704d98dae7fa048a8b7805d28
Abdullah Morrison <abdullahmorrison@gmail.com>
2025-07-01 09:40:05 -0400

    menu formatting & css tweaks

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -2,9 +2,9 @@ import menu from "@/lib/menu";
 
 export default function Menu() {
   return (
-    <div className="flex min-h-screen bg-primary py-10 px-2 md:px-10 ">
-      <main className="mx-auto flex w-full max-w-6xl flex-col items-center rounded-sm bg-secondary px-6 py-10 md:p-20 text-black">
-        <h1 className="text-5xl font-bold pb-20">Menu</h1>
+    <div className="flex min-h-screen items-center justify-center bg-primary px-2 py-10 md:px-10">
+      <main className="mx-auto flex w-full max-w-6xl flex-col items-center rounded-sm bg-secondary px-6 py-10 text-black md:p-20">
+        <h1 className="pb-20 text-5xl font-bold">Menu</h1>
         <div className="grid w-full grid-cols-1 gap-10 md:grid-cols-2">
           {menu.map((category) => (
             <div key={category.name} className="space-y-4">
@@ -14,12 +14,16 @@ export default function Menu() {
                   <li key={meal.name} className="pb-2">
                     <div className="flex items-center justify-between">
                       <h3 className="text-xl font-medium">{meal.name}</h3>
-                      <span className="text-lg font-bold">${meal.price.toFixed(2)}</span>
+                      <span className="text-lg font-bold">
+                        ${meal.price.toFixed(2)}
+                      </span>
                     </div>
                     {meal.note && (
-                      <span className="text-sm italic text-gray-500">*{meal.note}*</span>
+                      <span className="text-sm text-gray-500 italic">
+                        *{meal.note}*
+                      </span>
                     )}
-                    <p className="text-gray-700 mt-2">{meal.description}</p>
+                    <p className="mt-2 text-gray-700">{meal.description}</p>
                   </li>
                 ))}
               </ul>
