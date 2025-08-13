d6cee7493af1481459f345f63606819f7adb1c2d
Abdullah Morrison <abdullahmorrison@gmail.com>
2025-08-13 19:10:09 -0400

    changed testamonials

--- src/lib/testamonials.ts
+++ src/lib/testamonials.ts
@@ -5,14 +5,14 @@ const testimonials = [
     "testimonial": "Great food and customer services was above and beyond. Moroccan ambiance, family friendly and they have some divers Moroccan goodies as well. I'll definitely go there again, recommended"
   },
   {
-    "name": "Amanda Fraser",
-    "rating": 5,
-    "testimonial": "Happy I found this little gem of a place! The owners are incredibly friendly. Ordered chicken tajine. The serving size is good for 2 people. Came with fries, bread and a little side salad. Very flavourful and authentic Moroccan dish. Can’t wait to come back and try their other dishes."
+    "name": "Kevin Wlison",
+    "rating": 4,
+    "testimonial": "Food was good and my drink tasty apple soda  yum  price is 👍"
   },
   {
-    "name": "Cheng Dong",
+    "name": "Yahya Johnson Al-meko",
     "rating": 5,
-    "testimonial": "Moul Hanout is a family style homey Moroccan restaurant. The restaurant is filled with authentic Moroccan goodies and vibe, there is a blackboard containting all the restaurant's recommended dishes, honestly, you can't go wrong with any of them. We came as a group of International students, some of them Moroccan, they communicated swiftly with the owner, and what we got was a 5 star Moroccan cuisine experience."
+    "testimonial": "It’s a cozy, family-run place with a warm and welcoming atmosphere. If you’re looking for an authentic and satisfying meal, I highly recommend stopping by."
   },
   {
     "name": "Marouane AL",
